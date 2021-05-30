88dd0bf5587a506578e06a89e4c1c8e5b6eacec8
Babakailash <nathkailash2020@gmail.com>
2021-05-30 12:58:06 +0530

    Retrive:Count Employee Payroll

---
+++ src/main/java/com/jdbc/EmployeePayrollService.java
@@ -0,0 +1,16 @@
+package com.jdbc;
+import java.util.List;
+
+public class EmployeePayrollService {
+    public enum IOService {FILE_IO,DB_IO}
+
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {}
+
+    public List<EmployeePayrollData> readEmployeePayrollData(IOService iOservice) {
+        if (iOservice.equals(IOService.DB_IO))
+            this.employeePayrollList = new EmployeePayrollDBService().readData();
+        return this.employeePayrollList;
+    }
+}
---
+++ src/test/java/com/jdbc/test/EmployeePayrollServiceTest.java
@@ -0,0 +1,15 @@
+package com.jdbc.test;
+import com.jdbc.EmployeePayrollData;
+import com.jdbc.EmployeePayrollService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3,employeePayrollData.size());
+    }
+}
