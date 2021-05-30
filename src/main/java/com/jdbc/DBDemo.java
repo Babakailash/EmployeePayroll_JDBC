3441f0bffd3479aeaff555321531afa712672a6a
Babakailash <nathkailash2020@gmail.com>
2021-05-30 12:45:08 +0530

    Connection:JDBC Loader & Connection

--- src/main/java/com/jdbc/DBDemo.java
+++ src/main/java/com/jdbc/DBDemo.java
@@ -1,7 +1,37 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DBDemo {
     public static void main(String[] args) {
-        System.out.println("Welcome to MySql JDBC Connection");
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "root";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded ");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Not find the driver Path");
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to DATABase " +jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL,userName,password);
+            System.out.println("Connection Sucessful!!!:  " +connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println("  "+driverClass.getClass().getName());
+        }
     }
 }
